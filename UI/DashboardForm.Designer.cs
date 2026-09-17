c8eaca1b57305583530fbd7efeb3e97a6081b3d9
jhiancarl146-code <jhiancarl146@gmail.com>
2026-09-17 22:14:20 +0800

    Dashboard UI Created

---
+++ UI/DashboardForm.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace UI
+{
+    partial class DashboardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            panel1 = new Panel();
+            label6 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            panel2 = new Panel();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(70, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(322, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Student Enrollment System";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.DeepSkyBlue;
+            panel1.Controls.Add(button12);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(0, 2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1187, 61);
+            panel1.TabIndex = 1;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.DeepSkyBlue;
+            label6.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.ForeColor = SystemColors.Control;
+            label6.Location = new Point(1010, 7);
+            label6.Name = "label6";
+            label6.Size = new Size(45, 45);
+            label6.TabIndex = 4;
+            label6.Text = " │";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.DeepSkyBlue;
+            label4.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.Control;
+            label4.Location = new Point(908, 7);
+            label4.Name = "label4";
+            label4.Size = new Size(114, 45);
+            label4.TabIndex = 4;
+            label4.Text = "Admin";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.DeepSkyBlue;
+            label3.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.Control;
+            label3.Location = new Point(838, 7);
+            label3.Name = "label3";
+            label3.Size = new Size(64, 45);
+            label3.TabIndex = 3;
+            label3.Text = "👤";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.DeepSkyBlue;
+            label2.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.Control;
+            label2.Location = new Point(12, 7);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 45);
+            label2.TabIndex = 2;
+            label2.Text = "🎓";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Azure;
+            panel2.Controls.Add(button11);
+            panel2.Controls.Add(button10);
+            panel2.Controls.Add(button9);
+            panel2.Controls.Add(button7);
+            panel2.Controls.Add(button6);
+            panel2.Controls.Add(button5);
+            panel2.Controls.Add(button4);
+            panel2.Controls.Add(button3);
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(button1);
+            panel2.Location = new Point(0, 69);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(200, 643);
+            panel2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Azure;
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(0, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(200, 36);
+            button1.TabIndex = 3;
+            button1.Text = "Dasboard";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Azure;
+            button2.ForeColor = Color.Black;
+            button2.Location = new Point(0, 45);
+            button2.Name = "button2";
+            button2.Size = new Size(200, 36);
+            button2.TabIndex = 4;
+            button2.Text = "Student Account Search";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Azure;
+            button3.ForeColor = Color.Black;
+            button3.Location = new Point(0, 87);
+            button3.Name = "button3";
+            button3.Size = new Size(200, 36);
+            button3.TabIndex = 5;
+            button3.Text = "User Account Management";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Azure;
+            button4.ForeColor = Color.Black;
+            button4.Location = new Point(-3, 129);
+            button4.Name = "button4";
+            button4.Size = new Size(200, 36);
+            button4.TabIndex = 5;
+            button4.Text = "Student Management";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.Azure;
+            button5.ForeColor = Color.Black;
+            button5.Location = new Point(0, 171);
+            button5.Name = "button5";
+            button5.Size = new Size(200, 36);
+            button5.TabIndex = 6;
+            button5.Text = "Section Management";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.Azure;
+            button6.ForeColor = Color.Black;
+            button6.Location = new Point(0, 213);
+            button6.Name = "button6";
+            button6.Size = new Size(200, 36);
+            button6.TabIndex = 7;
+            button6.Text = "Student Enrollment";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Azure;
+            button7.ForeColor = Color.Black;
+            button7.Location = new Point(0, 255);
+            button7.Name = "button7";
+            button7.Size = new Size(200, 36);
+            button7.TabIndex = 6;
+            button7.Text = "Student Assessment";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.Azure;
+            button9.ForeColor = Color.Black;
+            button9.Location = new Point(3, 297);
+            button9.Name = "button9";
+            button9.Size = new Size(200, 36);
+            button9.TabIndex = 9;
+            button9.Text = "Payment/Cashiering";
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.Azure;
+            button10.ForeColor = Color.Black;
+            button10.Location = new Point(-3, 339);
+            button10.Name = "button10";
+            button10.Size = new Size(203, 36);
+            button10.TabIndex = 10;
+            button10.Text = "Payment History/ Reciepts";
+            button10.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.Azure;
+            button11.ForeColor = Color.Black;
+            button11.Location = new Point(-3, 381);
+            button11.Name = "button11";
+            button11.Size = new Size(203, 36);
+            button11.TabIndex = 11;
+            button11.Text = "Student Information Report";
+            button11.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.DeepSkyBlue;
+            button12.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button12.ForeColor = Color.White;
+            button12.Location = new Point(1045, 10);
+            button12.Name = "button12";
+            button12.Size = new Size(125, 43);
+            button12.TabIndex = 4;
+            button12.Text = "Logout";
+            button12.UseVisualStyleBackColor = false;
+            // 
+            // DashboardForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1182, 710);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            ForeColor = SystemColors.ControlLightLight;
+            Name = "DashboardForm";
+            Text = "DashboardForm";
+            TransparencyKey = Color.Black;
+            Load += DashboardForm_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private Panel panel1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label6;
+        private Panel panel2;
+        private Button button1;
+        private Button button6;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button11;
+        private Button button10;
+        private Button button9;
+        private Button button7;
+        private Button button12;
+    }
+}
---
+++ UI/DashboardForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class DashboardForm : Form
+    {
+        public DashboardForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DashboardForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
