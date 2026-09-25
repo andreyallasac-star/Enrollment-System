cf01bc8a3ee17e57f78cef1b00ae356a1aee3650
Leona Panaligan <Leona Panaligan@DESKTOP-P8E1RKS>
2026-09-26 00:55:15 +0800

    implement assessment form UI

---
+++ UI/AssessmentForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class AssessmentForm : Form
+    {
+        public AssessmentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
